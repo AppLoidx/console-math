ada477212906f1e4165dc696293946e56f13cb0a
AppLoid <apploidyakutsk@gmail.com>
2020-03-05 05:27:09 +0300

    javadoc

--- src/main/java/core/MatrixDecomposer.java
+++ src/main/java/core/MatrixDecomposer.java
@@ -6,6 +6,16 @@ import model.Matrix;
  * @author Arthur Kupriyanov on 03.03.2020
  */
 public interface MatrixDecomposer {
+    /**
+     * Decompose matrix like transform to pyramid matrix
+     * @param matrix source matrix
+     * @return transformed matrix. It doesn't might be clone
+     */
     Matrix decompose(Matrix matrix);
+
+    /**
+     * Get determinant calculated within process
+     * @return value of determinant
+     */
     float getDeterminant();
 }
