1b7dc6ffc57921049f9ecab76dd593792bde6e2b
Arthur Kupriyanov <apploidyakutsk@gmail.com>
2020-04-23 19:21:41 +0300

    utility system methods

---
+++ src/main/java/util/function/interfaces/DoubleFunction.java
@@ -0,0 +1,9 @@
+package util.function.interfaces;
+
+/**
+ * @author Arthur Kupriyanov on 23.04.2020
+ */
+@FunctionalInterface
+public interface DoubleFunction {
+    double apply(double x1, double x2);
+}
---
+++ src/main/java/util/function/interfaces/FourFunction.java
@@ -0,0 +1,9 @@
+package util.function.interfaces;
+
+/**
+ * @author Arthur Kupriyanov on 23.04.2020
+ */
+@FunctionalInterface
+public interface FourFunction {
+    double apply(double x1, double x2 ,double x3,double x4);
+}
